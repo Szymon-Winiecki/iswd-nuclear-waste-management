e8cdf59246c4443b1862a4932342832e2ad8e23a
Szymon-Winiecki <szymon.win4@gmail.com>
2024-03-20 18:41:35 +0100

    refactor code; extract UTA solver to separate class

---
+++ UTA_Solver.py
@@ -0,0 +1,109 @@
+import numpy as np
+import sys
+from pulp import *
+from matplotlib import pyplot as plt
+import os
+
+class UTA_Solver:
+    def __init__(self, data, criteria_direction, reference_ranking, num_characteristic_points):
+        self.data = data
+        self.criteria_direction = criteria_direction
+        self.reference_ranking = reference_ranking
+        self.num_characteristic_points = num_characteristic_points
+        self.num_criterion = self.data.shape[1]
+
+
+    def solve(self):
+
+        self.construct_LP_problem()
+
+        self.problem.solve()
+
+        self.characteristic_values = []
+        for c in range(len(self.utility_variables)):
+            self.characteristic_values.append([])
+            for var in self.utility_variables[c]:
+                self.characteristic_values[c].append(var.varValue)
+
+    def construct_LP_problem(self):
+
+        self.problem = LpProblem("Maximize_the_epsilon", LpMaximize)
+
+        # create epislon variable
+        epsilon = LpVariable("epsilon", lowBound=0, upBound=None)
+
+        # variables for partial utility functions
+        self.utility_variables = []
+        # characteristic points x coord
+        self.characteristic_thresholds = []
+
+        for c in range(self.num_criterion):
+            self.utility_variables.append([])
+            for i in range(self.num_characteristic_points):
+                self.utility_variables[c].append(LpVariable(f"w_{c}_{i}", lowBound=0, upBound=None))
+            self.characteristic_thresholds.append(np.linspace(self.data[:,c].min(), self.data[:,c].max(), self.num_characteristic_points))
+
+        # objective function
+        self.problem += epsilon, "Profit"
+
+        # reference ranking
+
+        def utility(c, x):
+            succ_theshold = 1
+            while x > self.characteristic_thresholds[c][succ_theshold]:
+                succ_theshold += 1
+            return self.utility_variables[c][succ_theshold - 1] + ( (x - self.characteristic_thresholds[c][succ_theshold - 1]) / (self.characteristic_thresholds[c][succ_theshold] - self.characteristic_thresholds[c][succ_theshold - 1]) ) * (self.utility_variables[c][succ_theshold] - self.utility_variables[c][succ_theshold - 1])
+
+
+        for e in self.reference_ranking:
+            if e[1] == "=":
+                self.problem += lpSum([utility(c, self.data[e[0], c]) for c in range(self.num_criterion)]) == lpSum([utility(c, self.data[e[2], c]) for c in range(self.num_criterion)])
+            elif e[1] == ">":
+                self.problem += lpSum([utility(c, self.data[e[0], c]) for c in range(self.num_criterion)]) >= lpSum([utility(c, self.data[e[2], c]) for c in range(self.num_criterion)]) + epsilon
+            elif e[1] == "<":
+                self.problem += lpSum([utility(c, self.data[e[2], c]) for c in range(self.num_criterion)]) >= lpSum([utility(c, self.data[e[0], c]) for c in range(self.num_criterion)]) + epsilon
+
+        # normalization
+        best_indices = list(map(lambda d : -1 if d == 'gain' else 0, self.criteria_direction))
+        worst_indices = list(map(lambda d : 0 if d == 'gain' else -1, self.criteria_direction))
+        self.problem += (lpSum([self.utility_variables[c][best_indices[c]] for c in range(self.num_criterion)]) == 1, "upper_normalization")
+        self.problem += (lpSum([self.utility_variables[c][worst_indices[c]] for c in range(self.num_criterion)]) == 0, "lower_normalization")
+
+        # monotonicity
+        for c in range(self.num_criterion):
+            if self.criteria_direction[c] == 'gain':
+                for i in range(1, len(self.utility_variables[c])):
+                    self.problem += self.utility_variables[c][i-1] <= self.utility_variables[c][i]
+            else:
+                for i in range(1, len(self.utility_variables[c])):
+                    self.problem += self.utility_variables[c][i-1] >=self. utility_variables[c][i]
+
+    def rank(self):
+        ranking = []
+        for i in range(self.data.shape[0]):
+            ranking.append((i, self.utility(self.data[i])))
+
+        ranking.sort(key = lambda x : -x[1])
+
+        return ranking
+
+    def partial_utility(self, x, characteristic_thresholds, characteristic_values):
+        succ_theshold = 1
+        while x > characteristic_thresholds[succ_theshold]:
+            succ_theshold += 1
+        return characteristic_values[succ_theshold - 1] + ( (x - characteristic_thresholds[succ_theshold - 1]) / (characteristic_thresholds[succ_theshold] - characteristic_thresholds[succ_theshold - 1]) ) * (characteristic_values[succ_theshold] - characteristic_values[succ_theshold - 1])
+
+    def utility(self, variant):
+        u = 0
+        for i in range(len(variant)):
+            u += self.partial_utility(variant[i], self.characteristic_thresholds[i],  self.characteristic_values[i])
+        return u
+
+    def plot_partial_utility(self, directory="results/plots"):
+
+        os.makedirs(directory, exist_ok=True)
+
+        for c in range(len(self.characteristic_thresholds)):
+            plt.cla()
+            plt.plot(self.characteristic_thresholds[c], self.characteristic_values[c])
+            plt.savefig(os.path.join(directory, f"U_{c}.png"))
--- nuclear-waste-management.py
+++ nuclear-waste-management.py
@@ -1,9 +1,10 @@
 import csv
 import numpy as np
-import sys
-from pulp import *
-from matplotlib import pyplot as plt
 import os
+
+import os
+
+from UTA_Solver import UTA_Solver
 
 def main():
     data_file = 'Nuclear waste management.csv'
@@ -21,20 +22,21 @@ def main():
 
     num_characteristic_points = 3
 
-    problem, characteristic_thresholds, utility_variables =  construct_LP_problem(data, criteria_direction, reference_ranking, num_characteristic_points)
+    solver =  UTA_Solver(data, criteria_direction, reference_ranking, num_characteristic_points)
 
-    target_val, characteristic_values = solve(problem, utility_variables)
+    solver.solve()
 
-    ranking = rank(data, characteristic_thresholds, characteristic_values)
+    ranking = solver.rank()
 
     ranking_str = ""
     for i in range(len(ranking)):
         ranking_str += f"{i:02d}. {ranking[i][0]} ({ranking[i][1]}) \n"
 
+    solver.plot_partial_utility()
+
+    os.makedirs("results", exist_ok=True)
     with open("results/ranking.txt", 'w') as file:
         file.write(ranking_str)
-
-    plot_partial_utility(characteristic_thresholds, characteristic_values)
 
 
 def read_data(filename):
@@ -51,116 +53,7 @@ def read_data(filename):
 
     return np.array(data, dtype=float)
 
-def construct_LP_problem(data, criteria_direction, reference_ranking, num_characteristic_points):
 
-    best_values = []
-    worst_values = []
-    for c in range(data.shape[1]):
-        if criteria_direction[c] == 'gain':
-            best_values.append(data[:,c].max())
-            worst_values.append(data[:,c].min())
-        elif criteria_direction[c] == 'cost':
-            best_values.append(data[:,c].min())
-            worst_values.append(data[:,c].max())
-        else:
-            sys.exit(f"unknown direction: {criteria_direction[c]}")
-
-    prob = LpProblem("Maximize_the_epsilon", LpMaximize)
-
-    # create epislon variable
-    epsilon = LpVariable("epsilon", lowBound=0, upBound=None)
-
-    # variables for partial utility functions
-    utility_variables = []
-    # characteristic points x coord
-    utility_thresholds = []
-
-    for c in range(data.shape[1]):
-        utility_variables.append([])
-        for i in range(num_characteristic_points):
-            utility_variables[c].append(LpVariable(f"w_{c}_{i}", lowBound=0, upBound=None))
-        utility_thresholds.append(np.linspace(data[:,c].min(), data[:,c].max(), num_characteristic_points))
-
-    # objective function
-    prob += epsilon, "Profit"
-
-    # reference ranking
-
-    def utility(c, x):
-        succ_theshold = 1
-        while x > utility_thresholds[c][succ_theshold]:
-            succ_theshold += 1
-        return utility_variables[c][succ_theshold - 1] + ( (x - utility_thresholds[c][succ_theshold - 1]) / (utility_thresholds[c][succ_theshold] - utility_thresholds[c][succ_theshold - 1]) ) * (utility_variables[c][succ_theshold] - utility_variables[c][succ_theshold - 1])
-
-
-    for e in reference_ranking:
-        if e[1] == "=":
-            prob += lpSum([utility(c, data[e[0], c]) for c in range(data.shape[1])]) == lpSum([utility(c, data[e[2], c]) for c in range(data.shape[1])])
-        elif e[1] == ">":
-            prob += lpSum([utility(c, data[e[0], c]) for c in range(data.shape[1])]) >= lpSum([utility(c, data[e[2], c]) for c in range(data.shape[1])]) + epsilon
-        elif e[1] == "<":
-            prob += lpSum([utility(c, data[e[2], c]) for c in range(data.shape[1])]) >= lpSum([utility(c, data[e[0], c]) for c in range(data.shape[1])]) + epsilon
-
-    # normalization
-    best_indices = list(map(lambda d : -1 if d == 'gain' else 0, criteria_direction))
-    worst_indices = list(map(lambda d : 0 if d == 'gain' else -1, criteria_direction))
-    prob += (lpSum([utility_variables[c][best_indices[c]] for c in range(data.shape[1])]) == 1, "upper_normalization")
-    prob += (lpSum([utility_variables[c][worst_indices[c]] for c in range(data.shape[1])]) == 0, "lower_normalization")
-
-    # monotonicity
-    for c in range(data.shape[1]):
-        if criteria_direction[c] == 'gain':
-            for i in range(1, len(utility_variables[c])):
-                prob += utility_variables[c][i-1] <= utility_variables[c][i]
-        else:
-            for i in range(1, len(utility_variables[c])):
-                prob += utility_variables[c][i-1] >= utility_variables[c][i]
-
-    return prob, utility_thresholds, utility_variables
-
-def solve(problem, utility_variables):
-
-    problem.solve()
-
-    utility_variables_values = []
-    for c in range(len(utility_variables)):
-        utility_variables_values.append([])
-        for var in utility_variables[c]:
-            utility_variables_values[c].append(var.varValue)
-
-    optimised = value(problem.objective)
-
-    return optimised, utility_variables_values
-
-def rank(data, characteristic_thresholds, characteristic_values):
-    ranking = []
-    for i in range(data.shape[0]):
-        ranking.append((i, utility(data[i], characteristic_thresholds, characteristic_values)))
-
-    ranking.sort(key = lambda x : -x[1])
-
-    return ranking
-
-def partial_utility(x, characteristic_thresholds, characteristic_values):
-    succ_theshold = 1
-    while x > characteristic_thresholds[succ_theshold]:
-        succ_theshold += 1
-    return characteristic_values[succ_theshold - 1] + ( (x - characteristic_thresholds[succ_theshold - 1]) / (characteristic_thresholds[succ_theshold] - characteristic_thresholds[succ_theshold - 1]) ) * (characteristic_values[succ_theshold] - characteristic_values[succ_theshold - 1])
-
-def utility(variant, characteristic_thresholds, characteristic_values):
-    u = 0
-    for i in range(len(variant)):
-        u += partial_utility(variant[i], characteristic_thresholds[i],  characteristic_values[i])
-    return u
-
-def plot_partial_utility(characteristic_thresholds, characteristic_values, directory="results/plots"):
-
-    os.makedirs(directory, exist_ok=True)
-
-    for c in range(len(characteristic_thresholds)):
-        plt.cla()
-        plt.plot(characteristic_thresholds[c], characteristic_values[c])
-        plt.savefig(os.path.join(directory, f"U_{c}.png"))
             
 
 main()
